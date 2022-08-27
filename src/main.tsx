c3600cc8580df562fffac90d16c3453598d032fc
Alexander Gabriel <alex.barbalex@gmail.com>
2022-08-27 10:57:17 +0200

    initiate

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,33 @@
-import './index.css';
-
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
+import { registerSW } from 'virtual:pwa-register';
+// see: https://github.com/fontsource/fontsource/blob/master/packages/roboto
+import '@fontsource/roboto-mono';
+import '@fontsource/roboto-mono/700.css';
+// see: https://github.com/fontsource/fontsource/tree/master/packages/roboto-mono
+import '@fontsource/roboto';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+import './index.css';
 
 import App from './App';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root'),
-);
+// https://vite-plugin-pwa.netlify.app/guide/prompt-for-update.html#runtime
+registerSW({
+  onNeedRefresh() {
+    if (
+      window.confirm('capturing.app neu laden, um die neuste Version zu installieren?')
+    ) {
+      // TODO: empty everything but the login?
+      // seems that is what is happening
+      window.location.reload(true);
+    }
+  },
+  onOfflineReady() {
+    console.log('the service worker is offline ready');
+  },
+});
+const container = document.getElementById('root');
+const root = createRoot(container);
+
+root.render(<App />);
