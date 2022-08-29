cf72dc1cef8318901de4f61c77bbcdffe9c58db3
Alexander Gabriel <alex.barbalex@gmail.com>
2022-08-29 21:35:51 +0200

    initiate

--- src/utils/fetchFromServer/processTable.ts
+++ src/utils/fetchFromServer/processTable.ts
@@ -23,35 +23,36 @@ const processSubscriptionResult = async ({
   }
   setInitiallyQuerying(table)
 
-  const collection = db.get(table)
-  const incomingIds = dataToCheck.map((d) => d.id)
+  // const collection = db.get(table)
+  // const incomingIds = dataToCheck.map((d) => d.id)
 
-  let objectsOfToUpdate = []
-  try {
-    objectsOfToUpdate = [] // TODO: dexie
-    // await db
-    //   .get(table)
-    //   .query(Q.where('id', Q.oneOf(incomingIds)))
-    //   .fetch()
-  } catch {}
-  let objectsOfIncoming = []
-  try {
-    objectsOfIncoming = [] // TODO: dexie
-    // await db
-    //   .get(table)
-    //   .query(Q.where('id', Q.oneOf(incomingIds)))
-    //   .fetch()
-  } catch {}
-  const existingIds = objectsOfIncoming.map((d) => d.id)
-  const missingIds = incomingIds.filter((d) => !existingIds.includes(d))
-  const dataToCreateObjectsFrom = dataToCheck.filter((d) =>
-    missingIds.includes(d.id),
-  )
-  // only if remote changed after local
-  const objectsToUpdate = objectsOfToUpdate.filter((o) => {
-    const dat = stripTypename(dataToCheck.find((d) => d.id === o.id))
-    return !Object.entries(dat).every(([key, value]) => isEqual(value, o[key]))
-  })
+  // let objectsOfToUpdate = []
+  // try {
+  //   objectsOfToUpdate = dexie[table].where('id').anyOf(incomingIds).toArray()
+  //   // await db
+  //   //   .get(table)
+  //   //   .query(Q.where('id', Q.oneOf(incomingIds)))
+  //   //   .fetch()
+  // } catch {}
+  // let objectsOfIncoming = []
+  // try {
+  //   objectsOfIncoming = [] // TODO: dexie
+  //   objectsOfIncoming = dexie[table].where('id').anyOf(incomingIds).toArray()
+  //   // await db
+  //   //   .get(table)
+  //   //   .query(Q.where('id', Q.oneOf(incomingIds)))
+  //   //   .fetch()
+  // } catch {}
+  // const existingIds = objectsOfIncoming.map((d) => d.id)
+  // const missingIds = incomingIds.filter((d) => !existingIds.includes(d))
+  // const dataToCreateObjectsFrom = dataToCheck.filter((d) =>
+  //   missingIds.includes(d.id),
+  // )
+  // // only if remote changed after local
+  // const objectsToUpdate = objectsOfToUpdate.filter((o) => {
+  //   const dat = stripTypename(dataToCheck.find((d) => d.id === o.id))
+  //   return !Object.entries(dat).every(([key, value]) => isEqual(value, o[key]))
+  // })
   console.log('processSubscriptionResult:', {
     table,
     toUpdate: objectsToUpdate.length,
@@ -62,45 +63,48 @@ const processSubscriptionResult = async ({
   // reason: indexedDB creates indexes right after a table was imported
   // need to stagger imports to keep ui responsive between them
   setTimeout(async () => {
-    try {
-      await db.write(async () => {
-        if (objectsToUpdate.length || dataToCreateObjectsFrom.length) {
-          await db.batch(
-            ...objectsToUpdate.map((object) => {
-              const thisObjectsData = dataToCheck.find(
-                (d) => d.id === object.id,
-              )
+    // TODO: strip out typeName
+    await dexie[table].bulkPut(dataToCheck)
+    setInitiallyQueried({ table })
+    // try {
+    //   await db.write(async () => {
+    //     if (objectsToUpdate.length || dataToCreateObjectsFrom.length) {
+    //       await db.batch(
+    //         ...objectsToUpdate.map((object) => {
+    //           const thisObjectsData = dataToCheck.find(
+    //             (d) => d.id === object.id,
+    //           )
 
-              return object.prepareUpdate((ob) => {
-                Object.keys(thisObjectsData)
-                  .filter((key) => !['id', '__typename'].includes(key))
-                  .forEach((key) => {
-                    if (!isEqual(ob[key], thisObjectsData[key])) {
-                      ob[key] = thisObjectsData[key]
-                      // for indexable boolean fields also set _indexable field
-                      if (
-                        typeof thisObjectsData[key] === 'boolean' &&
-                        `${key}_indexable` in ob
-                      ) {
-                        ob[`${key}_indexable`] = thisObjectsData[key] ? 1 : 0
-                      }
-                    }
-                  })
-              })
-            }),
-            // prepareCreateFromDirtyRaw replaces watermelon's id with vermehrung's
-            ...dataToCreateObjectsFrom.map((d) =>
-              collection.prepareCreateFromDirtyRaw(parseComplexFieldsForWm(d)),
-            ),
-          )
-          setInitiallyQueried({ table })
-        }
-        setInitiallyQueried({ table })
-      })
-    } catch (error) {
-      console.log('Error in processSubscriptionResult > db.action:', error)
-      setInitiallyQueried({ table })
-    }
+    //           return object.prepareUpdate((ob) => {
+    //             Object.keys(thisObjectsData)
+    //               .filter((key) => !['id', '__typename'].includes(key))
+    //               .forEach((key) => {
+    //                 if (!isEqual(ob[key], thisObjectsData[key])) {
+    //                   ob[key] = thisObjectsData[key]
+    //                   // for indexable boolean fields also set _indexable field
+    //                   if (
+    //                     typeof thisObjectsData[key] === 'boolean' &&
+    //                     `${key}_indexable` in ob
+    //                   ) {
+    //                     ob[`${key}_indexable`] = thisObjectsData[key] ? 1 : 0
+    //                   }
+    //                 }
+    //               })
+    //           })
+    //         }),
+    //         // prepareCreateFromDirtyRaw replaces watermelon's id with vermehrung's
+    //         ...dataToCreateObjectsFrom.map((d) =>
+    //           collection.prepareCreateFromDirtyRaw(parseComplexFieldsForWm(d)),
+    //         ),
+    //       )
+    //       setInitiallyQueried({ table })
+    //     }
+    //     setInitiallyQueried({ table })
+    //   })
+    // } catch (error) {
+    //   console.log('Error in processSubscriptionResult > db.action:', error)
+    //   setInitiallyQueried({ table })
+    // }
   })
   setLastUpdated({ table })
 }
