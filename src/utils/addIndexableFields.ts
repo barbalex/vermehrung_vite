dd529abfa7805bf301ba9d6555245ce25ba45d13
Alexander Gabriel <alex.barbalex@gmail.com>
2022-09-26 13:14:43 +0200

    catch

--- src/utils/addIndexableFields.ts
+++ src/utils/addIndexableFields.ts
@@ -1,9 +1,9 @@
-import indexableFields from './indexableFields' 
+import indexableFields from './indexableFields'
 import booleanToInteger from './booleanToInteger'
 
 const addIndexableFields = ({ table, object }) => {
   Object.entries(object).forEach(([key, value]) => {
-    if (indexableFields[table]?.includes(key)) {
+    if ((indexableFields[table] ?? []).includes(key)) {
       object[`${key}_indexable`] = booleanToInteger(value)
     }
   })
